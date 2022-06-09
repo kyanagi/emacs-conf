664b7a6f6f1b741dfb4f54096fd20ae28d5786a5
Kouhei Yanagita <yanagi@shakenbu.org>
2022-06-09 11:56:43 +0900

    開いているファイルとバッファをリネームする my/renamed-current-buffer-file を追加

--- init.el
+++ init.el
@@ -290,6 +290,26 @@ XDG_DATA_HOMEが設定されていれば$XDG_DATA_HOME/emacs、
     :hook
     (switch-buffer-functions . my/toggle-auto-save-visited-mode-for-buffer)
     )
+
+  ;; 開いているファイルとバッファをリネームする
+  ;; https://stackoverflow.com/questions/384284/how-do-i-rename-an-open-file-in-emacs
+  (defun my/rename-current-buffer-file ()
+    "Renames current buffer and file it is visiting."
+    (interactive)
+    (let* ((name (buffer-name))
+           (filename (buffer-file-name))
+           (basename (file-name-nondirectory filename)))
+      (if (not (and filename (file-exists-p filename)))
+          (error "Buffer `%s' is not visiting a file!" name)
+        (let ((new-name (read-file-name "New name: " (file-name-directory filename) basename nil basename)))
+          (if (get-buffer new-name)
+              (error "A buffer named `%s' already exists!" new-name)
+            (rename-file filename new-name 1)
+            (rename-buffer new-name)
+            (set-visited-file-name new-name)
+            (set-buffer-modified-p nil)
+            (message "File `%s' successfully renamed to `%s'"
+                     name (file-name-nondirectory new-name)))))))
   )
 
 (leaf save-place
