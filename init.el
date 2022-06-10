dce7442e09c3d9f00b8bbdc283b4dceccd185aca
Kouhei Yanagita <yanagi@shakenbu.org>
2022-06-10 10:04:09 +0900

    corfuの導入

--- init.el
+++ init.el
@@ -583,6 +583,49 @@ XDG_DATA_HOMEが設定されていれば$XDG_DATA_HOME/emacs、
     :global-minor-mode t)
   )
 
+(leaf corfu
+  :ensure t
+  :require t
+  :custom
+  (corfu-cycle . t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto . t)                 ;; Enable auto completion
+  (corfu-auto-delay . 0.01)                 ;; Enable auto completion
+  (corfu-count . 15)                        ;; show more candidates
+  (corfu-auto-prefix . 3)
+  (corfu-max-width . 150)               ;; max width of corfu completion UI
+  (corfu-commit-predicate . #'my:corfu-commit-predicate)   ;; Do not commit selected candidates on next input
+  (corfu-quit-no-match . 'separator)
+  (corfu-separator . ?\s)
+  (corfu-preview-current . t)    ;; Disable current candidate preview
+  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
+  (corfu-echo-documentation . nil) ;; Disable documentation in the echo area
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  :bind
+  (:corfu-map
+   ("SPC" . corfu-insert-separator))
+  :preface
+  ;; from https://github.com/minad/corfu/wiki
+  (defun my:corfu-commit-predicate ()
+    "Auto-commit candidates if
+
+  1. A \".\" is typed, except after a SPACE.
+  2. A key is typed after corfu-complete
+  3. A selection was made, aside from entering SPACE.
+  "
+    (cond
+     ((seq-contains-p (this-command-keys-vector) ?.)
+      (or (string-empty-p (car corfu--input))
+          (not (string= (substring (car corfu--input) -1) " "))))
+
+     ((equal last-command #'corfu-complete)
+      (or (string-empty-p (car corfu--input))
+          (not (string= (substring (car corfu--input) -1) " "))))
+
+     ((/= corfu--index corfu--preselect) ; a selection was made
+      (not (seq-contains-p (this-command-keys-vector) ? )))))
+  :config
+  (global-corfu-mode))
+
 (leaf migemo
   :ensure t
   :custom
